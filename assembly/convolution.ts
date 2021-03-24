ff1b897c0761a6b295007d733a030cb012f75deb
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-03-24 18:12:57 +0900

    Optimize convolution

--- assembly/convolution.ts
+++ assembly/convolution.ts
@@ -27,15 +27,17 @@ function safe_mod(x: i32, m: i32): i32 {
 }
 
 function add_mod(x: i32, y: i32, m: i32): i32 {
-  x += y;
-  if (x >= m) x -= m;
-  return x;
+  return (x + y) % m;
+  // x += y;
+  // if (x >= m) x -= m;
+  // return x;
 }
 
 function sub_mod(x: i32, y: i32, m: i32): i32 {
-  x -= y;
-  if (x < 0) x += m;
-  return x;
+  return (x - y + m) % m;
+  // x -= y;
+  // if (x < 0) x += m;
+  // return x;
 }
 
 function mul_mod(x: i32, y: i32, m: i32): i32 {
@@ -56,6 +58,41 @@ function butterfly(a_ptr: usize, h: i32, mod: i32, sum_e_ptr: usize): void {
         const r = mul_mod(a[i + offset + p], now, mod);
         a[i + offset] = add_mod(l, r, mod);
         a[i + offset + p] = sub_mod(l, r, mod);
+      }
+      now = mul_mod(now, sum_e[bsf(~u32(s))], mod);
+    }
+  }
+}
+
+function butterfly2(
+  a_ptr: usize,
+  b_ptr: usize,
+  h: i32,
+  mod: i32,
+  sum_e_ptr: usize
+): void {
+  const a = raw<i32>(a_ptr);
+  const b = raw<i32>(b_ptr);
+  const sum_e = raw<i32>(sum_e_ptr);
+  for (let ph = 1; ph <= h; ph++) {
+    const w = 1 << (ph - 1);
+    const p = 1 << (h - ph);
+    let now = 1;
+    for (let s = 0; s < w; s++) {
+      const offset = s << (h - ph + 1);
+      for (let i = 0; i < p; i++) {
+        {
+          const l = a[i + offset];
+          const r = mul_mod(a[i + offset + p], now, mod);
+          a[i + offset] = add_mod(l, r, mod);
+          a[i + offset + p] = sub_mod(l, r, mod);
+        }
+        {
+          const l = b[i + offset];
+          const r = mul_mod(b[i + offset + p], now, mod);
+          b[i + offset] = add_mod(l, r, mod);
+          b[i + offset + p] = sub_mod(l, r, mod);
+        }
       }
       now = mul_mod(now, sum_e[bsf(~u32(s))], mod);
     }
@@ -105,8 +142,9 @@ export function convolution(
   for (let i = 0; i < z; i++) {
     b[i] = safe_mod(b[i], mod);
   }
-  butterfly(a_ptr, h, mod, sum_e_ptr);
-  butterfly(b_ptr, h, mod, sum_e_ptr);
+  // butterfly(a_ptr, h, mod, sum_e_ptr);
+  // butterfly(b_ptr, h, mod, sum_e_ptr);
+  butterfly2(a_ptr, b_ptr, h, mod, sum_e_ptr);
   for (let i = 0; i < z; i++) {
     a[i] = mul_mod(a[i], b[i], mod);
   }
